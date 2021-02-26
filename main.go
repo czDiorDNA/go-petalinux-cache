c04dce44c03c4e290ac1920ffe2768a05b75592e
chenzhi <22946187@qq.com>
2021-02-27 00:30:29 +0800

    init

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/extensions"
+)
+
+const Version = "rel-v2018.2"
+const BaseURL = "http://petalinux.xilinx.com/sswreleases/" + Version + "/"
+
+func GetRootDir() string {
+	root, _ := os.Getwd()
+	return path.Join(root, "downloads", Version)
+}
+
+func IsFile(file string) bool {
+	f, e := os.Stat(file)
+	if e != nil {
+		return false
+	}
+	return !f.IsDir()
+}
+
+func IsDir(file string) bool {
+	f, e := os.Stat(file)
+	if e != nil {
+		return false
+	}
+	return f.IsDir()
+}
+
+func main() {
+	c := colly.NewCollector(func(collector *colly.Collector) {
+		collector.AllowURLRevisit = true
+		collector.SetRequestTimeout(100 * time.Second)
+		extensions.RandomUserAgent(collector)
+	})
+	downC := c.Clone()
+	c.OnHTML("#indexlist", func(e *colly.HTMLElement) {
+		e.ForEach("tr", func(index int, child *colly.HTMLElement) {
+			// 跳过Table头和返回上一级
+			if index <= 1 {
+				return
+			}
+			
+			url := child.DOM.Find(".indexcolname>a").Text()
+			url = e.Request.URL.String() + url
+			size := child.DOM.Find(".indexcolsize").Text()
+			size = strings.Trim(size, " ")
+			// 代表是目录
+			if size == "-" {
+				e.Request.Visit(url)
+			} else {
+				downC.Visit(url)
+			}
+		})
+	})
+	c.OnError(func(resp *colly.Response, err error) {
+		fmt.Println("err: ", resp.Request.URL.String(), err)
+		resp.Request.Visit(resp.Request.URL.String())
+	})
+
+	downC.OnResponse(func(r *colly.Response) {
+		filePath := strings.Replace(r.Request.URL.String(), BaseURL, "", -1)
+		filePath = path.Join(GetRootDir(), filePath)
+		if !IsDir(path.Dir(filePath)) {
+			os.MkdirAll(path.Dir(filePath), 0777)
+		}
+
+		res, err := http.Get(r.Request.URL.String())
+		if err != nil {
+			downC.Visit(r.Request.URL.String())
+			return
+		}
+		f, err := os.Create(filePath)
+		if err != nil {
+			downC.Visit(r.Request.URL.String())
+			return
+		}
+		io.Copy(f, res.Body)
+	})
+
+	c.Visit(BaseURL)
+}
